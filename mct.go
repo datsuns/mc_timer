deb2ee58e2a8ddd7008cc65cc3b14b588eab6a07
datsuns <the.skeleton7@gmail.com>
2021-02-24 22:25:33 +0900

    update layout

--- mct.go
+++ mct.go
@@ -12,8 +12,11 @@ import (
 )
 
 var TotalWaitTimes = 6
-var MinutesToWait = 10
-var WaitToMessage = time.Minute * 10
+var MinutesPerWait time.Duration = 1
+
+var WaitToMessage = time.Minute * MinutesPerWait
+
+//var WaitToMessage = time.Second * 10
 
 var JavaExec = "java"
 var ServerJarName = "server.jar"
@@ -45,11 +48,11 @@ func runserver() {
 	go func() {
 		myin.Scan()
 		fmt.Println("wait start")
-		elapsed := 0
+		var elapsed time.Duration = 0
 		for i := 0; i < TotalWaitTimes; i++ {
 			time.Sleep(WaitToMessage)
-			elapsed += MinutesToWait
-			text := fmt.Sprintf("%d minutes\n", elapsed)
+			elapsed += MinutesPerWait
+			text := fmt.Sprintf("/say %d minutes\n", elapsed)
 			fmt.Println("exec say")
 			writer.Write([]byte(text))
 		}
@@ -74,14 +77,23 @@ func notify_error_when_java_not_found() {
 }
 
 func app_on_tview() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
+			SetTextAlign(tview.AlignCenter).
+			SetText(text)
+	}
+	grid := tview.NewGrid().
+		SetBorders(true).
+		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
+		AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
+
+	if err := tview.NewApplication().SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
 	fmt.Println("hello")
-	runserver()
-	//app_on_tview()
+	//runserver()
+	app_on_tview()
 }
